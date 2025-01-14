9f2ec453fe3b1fb2ca6be97687446eeeb4c528c2
marcelotc <marceloteixeiracortes@gmail.com>
2025-01-13 22:16:08 -0300

    dynamically update copyright year in footer

--- src/components/main-footer.tsx
+++ src/components/main-footer.tsx
@@ -1,7 +1,9 @@
 export default function MainFooter() {
     return (
         <footer className={`bottom-0 h-8 w-full shrink-0 items-center justify-center p-10 md:px-6 bg-[#1b1b1d]`}>
-            <p className="text-sm text-gray-500 dark:text-gray-400 text-center">© 2024 Front-end Foundation</p>
+            <p className="text-sm text-gray-500 dark:text-gray-400 text-center">
+                © {new Date().getFullYear()} Front-end Foundation
+            </p>
         </footer>
     );
 }
